6a903cd5e6dbdfb18a42590db1f3e4f9f219d5b6
p jagdish barik <pjagdishbarik3@gmail.com>
2025-08-21 12:36:06 +0530

    first commit

---
+++ flipcart.js
@@ -0,0 +1,28 @@
+// let image=document.querySelector(".slider-image");
+// let translate=0;
+
+//  setInterval(()=>{
+//     if(translate>=0&& translate<500){
+//         translate+=100;
+//     }
+//     else{
+//         translate=0  ;
+//       }
+//       image.style.trasition="2s";
+//       image.style.transform=`translateX(${-translate}%)`;
+// },1000);
+// function right(){
+//     if(translate<400){
+//         translate+=100
+//         image.style.trasition="1s";
+//             image.style.transform=`translateX(${-translate}%)`
+//     }
+// }
+
+// function let(){
+//     if(translate<400){
+//         translate+=100
+//         image.style.trasition="1s";
+//             image.style.transform=`translateX(${-translate}%)`
+//     }
+// }
